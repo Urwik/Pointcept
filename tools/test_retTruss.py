7d59a58d19338fe111740eef4bcebebd9b49c3fd
FJSoler_arvc <f.soler@umh.es>
2024-07-25 09:21:47 +0200

    first commit

---
+++ tools/test_retTruss.py
@@ -0,0 +1,44 @@
+"""
+Main Testing Script
+
+Author: Xiaoyang Wu (xiaoyang.wu.cs@gmail.com)
+Please cite our work if the code is helpful to you.
+"""
+
+import sys
+sys.path.append("/home/arvc/Fran/workSpaces/nn_ws/binary_segmentation/repos/Pointcept")
+
+
+from pointcept.engines.defaults import (
+    default_argument_parser,
+    default_config_parser,
+    default_setup,
+)
+from pointcept.engines.test import TESTERS
+from pointcept.engines.launch import launch
+
+
+def main_worker(cfg):
+    cfg = default_setup(cfg)
+    tester = TESTERS.build(dict(type=cfg.test.type, cfg=cfg))
+    tester.test()
+
+
+def main():
+    args = default_argument_parser().parse_args()
+    cfg = default_config_parser(args.config_file, args.options)
+
+    launch(
+        main_worker,
+        num_gpus_per_machine=args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        cfg=(cfg,),
+    )
+
+if __name__ == "__main__":
+
+    sys.argv.extend(["--config-file", "exp/retTruss/nxnynz/config.py"])
+    sys.argv.extend(["--options", "save_path=exp/retTruss/nxnynz", "weight=exp/retTruss/nxnynz/model/model_best.pth"])
+    main()
